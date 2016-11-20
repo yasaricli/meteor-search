1fe0f42e3f43d55bbdd2b81e41d4211dc72e916c
Yasar icli <hello@yasaricli.com>
2016-11-20 15:33:46 +0300

    add git and readme package.

--- package.js
+++ package.js
@@ -1,10 +1,13 @@
 Package.describe( {
   name: 'yasaricli:search',
   summary: 'Search the Mongo Collection using Fuse.',
-  version: '0.0.1'
+  version: '0.0.1',
+  git: "https://github.com/yasaricli/meteor-search.git",
+  documentation: "README.md"
 });
 
 Package.onUse((api) => {
+  api.versionsFrom('1.4.2');
   api.use("ecmascript");
   api.mainModule('search.js', 'server');
 });
