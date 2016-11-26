cf77c952b0db47572e6cb9ab50cc30e611396b50
Yasar icli <hello@yasaricli.com>
2016-11-26 12:48:12 +0300

    Bump 0.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe( {
   name: 'yasaricli:search',
   summary: 'Search the Mongo Collection using Fuse.',
-  version: '0.0.1',
+  version: '0.0.2',
   git: "https://github.com/yasaricli/meteor-search.git",
   documentation: "README.md"
 });
@@ -10,6 +10,6 @@ Package.onUse((api) => {
   api.versionsFrom('1.4.2');
   api.use("ecmascript");
 
-  // server and client 
+  // server and client
   api.mainModule('search.js');
 });
